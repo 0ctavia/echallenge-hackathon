03794cd4f13a493cb2f905b86c31d10a0606e7d4
Nassim Koceir <nassim.koceir@gmail.com>
2019-11-24 06:08:00 +0100

    adding dialogflow to page

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,9 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>be-IT</title>
+    <title>be.IT</title>
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <p>Hello world</p>
+	<iframe
+		allow="microphone;"
+		width="350"
+		height="430"
+		src="https://console.dialogflow.com/api-client/demo/embedded/46db9b3e-25c5-4462-b7ed-4b47303c0cf1"
+		id="chatbot">
+	</iframe>
 </body>
 </html>
